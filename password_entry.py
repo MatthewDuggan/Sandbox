0e04027fcefd384f9870c99ab1cbcf39e2cd3ea3
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-08-23 11:40:25 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,4 +1,10 @@
 """Matthew Duggan"""
 
-
+password = input("input your password: ")
+MIN_LENGTH = 10
+if len(password) >= MIN_LENGTH:
+    for char in password:
+        print("*",end="")
+else:
+    print("The password is shorter than the required length")
 
